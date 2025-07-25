2583a63d8cb40bb8138a6c31d8ec91c33060ae0a
hmuaz <92471130+hmuaz@users.noreply.github.com>
2025-07-25 13:15:07 +0300

    aim mode

--- Assets/StarterAssets/ThirdPersonController/Scripts/ThirdPersonShooterController.cs
+++ Assets/StarterAssets/ThirdPersonController/Scripts/ThirdPersonShooterController.cs
@@ -2,11 +2,18 @@ using System;
 using Cinemachine;
 using StarterAssets;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class ThirdPersonShooterController : MonoBehaviour
 {
     [SerializeField]
     private CinemachineVirtualCamera _aimVirtualCamera;
+    
+    [SerializeField]
+    private LayerMask _aimColliderMask;
+    
+    [SerializeField] 
+    private Transform _debugTransform;
 
     private StarterAssetsInputs _input;
 
@@ -17,13 +24,33 @@ public class ThirdPersonShooterController : MonoBehaviour
 
     void Update()
     {
+        Vector3 mouseWorldPosition = Vector3.zero;
+        
+        
+        
+        Ray ray = Camera.main.ScreenPointToRay(Mouse.current.position.ReadValue());
+        if (Physics.Raycast(ray, out RaycastHit raycastHit, 999f, _aimColliderMask))
+        {
+            _debugTransform.position = raycastHit.point;
+            mouseWorldPosition = raycastHit.point;
+        }
+        
+        
+        
         if (_input.aim)
         {
             _aimVirtualCamera.gameObject.SetActive(true);
+            
+            Vector3 worldAimTarget = mouseWorldPosition;
+            worldAimTarget.y = transform.position.y;
+            Vector3 aimDirection = (worldAimTarget - transform.position).normalized;
+            
+            transform.forward = Vector3.Lerp(transform.forward, aimDirection, Time.deltaTime * 20f);
         }
         else
         {
             _aimVirtualCamera.gameObject.SetActive(false);
         }
     }
+    
 }
